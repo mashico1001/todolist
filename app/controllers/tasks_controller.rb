38016001ec9cc8877e7b65fc6415db128b28a474
mashico1001 <wabumpr@gmail.com>
2022-05-23 12:28:23 +0000

    [Add]タスクの期限切れ、Doneの設定

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,8 +1,11 @@
 class TasksController < ApplicationController
 
   def index
-    @tasks = Task.all
     @task = Task.new
+
+    @tasks = Task.where(completion_at:nil).where("deadline_at >= ?", Time.now).order(:deadline_at)
+    @tasks_done = Task.where.not(completion_at:nil)
+    @tasks_expired = Task.where(completion_at:nil).where("deadline_at <= ?", Time.now)
   end
 
   def create
@@ -20,15 +23,23 @@ class TasksController < ApplicationController
   end
 
   def edit
+    @task = Task.find(params[:id])
   end
 
   def update
   end
 
   def destroy
+    @task = Task.find(params[:id])
+    @task.destroy
+    redirect_to tasks_path
   end
 
   def done
+    @now = Time.now
+    @task = Task.find(params[:id])
+    @task.update(completion_at: @now)
+    redirect_to :action => "index"
   end
 
   private
